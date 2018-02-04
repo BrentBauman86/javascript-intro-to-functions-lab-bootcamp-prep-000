ee484bc0642bf3ba5a49cf028f8c0b52fe6b2597
Brent Bauman <brentbauman86@gmail.com>
2018-02-04 17:39:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function shout(string){
   return string.toUpperCase()
 }
+<<<<<<< HEAD
 function whisper(string){
   return string.toLowerCase()
 }
@@ -9,3 +10,5 @@ function sayHiToGrandma(string){
   console.log("I can't hear you!")
 
 }
+=======
+>>>>>>> ad2ff96ffd00059ea7ec47a0e4b7136f3c585f68
